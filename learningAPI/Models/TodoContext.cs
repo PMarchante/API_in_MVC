e882652752b1f16f1f6c4c90ae8173df7c749e6e
Pedro Marchante <pmarc560@gmail.com>
2019-08-14 20:45:48 -0400

    Add project files.

---
+++ learningAPI/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace learningAPI.Models
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> dumb)
+            : base(dumb) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
